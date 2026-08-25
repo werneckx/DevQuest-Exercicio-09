862eb27fea78a5045ec9eb85d294ccc09ccbee02
Edson Rodrigues <er4273270@gmail.com>
2026-08-25 11:18:10 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const hospedagem = confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+if (hospedagem === true) {
+    alert("Ótimo! Nós temos as melhores camas de toda a região!")
+} else {
+    alert("Que pena! Você parecia ser uma pessoa mais legal")
+}
